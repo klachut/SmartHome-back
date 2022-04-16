0fb03163ecefa7d8947b54290163559a3e4d2d65
Xarinia <katarzynalachut12@gmail.com>
2022-04-16 10:08:33 +0200

    v1.0

---
+++ backend/controllers/sensors.js
@@ -0,0 +1,29 @@
+const Sensor = require('../models/Sensor');
+
+const getAllSensors = async (req, res) => {
+	const sensors = await Sensor.find({})
+	res.status(200).json(sensors);
+};
+
+const addSensor = async (req, res) => {
+    const body = req.body;
+    const newSensor = await Sensor.create(body);
+    return res.status(201).json(newSensor)
+}
+
+const updateSensor = async (req, res) => {
+ 
+    const {isActivated} = req.body
+    const id = req.params.id;
+
+    const updatedSensor = await Sensor.findByIdAndUpdate(id, {isActivated}, { new: true });
+    return res.status(200).json(updatedSensor)
+}
+
+const removeSensor = async (req, res) => {
+    const id = req.params.id;
+    await Sensor.findOneAndDelete(id)
+    return res.status(200).end()
+}
+
+module.exports = {getAllSensors, addSensor, updateSensor, removeSensor}
